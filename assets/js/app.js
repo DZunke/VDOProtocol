cfbe820a86a6418347834ad027404fb96823fdbb
Denis Zunke <denis.zunke@gmail.com>
2019-03-20 14:58:36 +0100

    Restructure Font Awesome usage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('tabler-ui/dist/assets/css/dashboard.css');
 require('../css/app.scss');
+require('../css/fontawesome.min.css');
+require('../css/brands.min.css');
+require('../css/solid.css');
 
 const $ = require('jquery');
 global.$ = global.jQuery = $;
--- assets/js/core.js
+++ assets/js/core.js
@@ -1,82 +1,89 @@
-$(document).ready(function () {
-    $('a, input[type=submit]').on('click', function ($e) {
-        $.Loader('check', this);
-    });
+(function ($) {
 
-    $('form').on('submit', function () {
-        $.Loader('check', this);
-    });
-
-    $(document).on('keydown', function ($e) {
-        if (($e.which || $e.keyCode) === 116) { // F5
+    $(document).ready(function () {
+        $('a, input[type=submit]').on('click', function ($e) {
             $.Loader('check', this);
-        }
-    });
-});
+        });
 
-$.Loader = function ($action, $element) {
-    switch ($action.toLowerCase()) {
-        case "check":
-            return _Check($element);
-        case "show":
-            return _Show();
-        case "hide":
-            return _Hide();
-    }
-};
+        $('form').on('submit', function () {
+            $.Loader('check', this);
+        });
 
-function _Check($element) {
-    if ($($element).hasClass('no-loader')) {
-        return;
-    }
-
-    var $checkForm = $($element).data('check-form');
-    if ($checkForm !== undefined) {
-        $form = $('form[name="' + $($element).data('check-form') + '"]');
-        if ($form.length === 0 || $form[0].noValidate || $form[0].checkValidity()) {
-            $.Loader("show");
-            $form[0].submit();
-        }
-
-        return;
-    }
-
-    $.Loader("show");
-}
-
-function _Show() {
-    if ($('.js-loader').length > 0) {
-        return;
-    }
-
-    var $loaderDiv = $("<div>", {
-        "class": "js-loader modal-backdrop",
-        "css": {
-            "display": "flex",
-            "flex-direction": "column",
-            "align-items": "center",
-            "justify-content": "center",
-            "opacity": "0.8"
-        }
+        $(document).on('keydown', function ($e) {
+            if (($e.which || $e.keyCode) === 116) { // F5
+                $.Loader('check', this);
+            }
+        });
     });
 
-    $('<i>', {
-        'class': 'fa fa-spinner fa-pulse fa-3x fa-fw'
-    }).appendTo($loaderDiv);
+    $.Loader = function ($action, $element) {
+        switch ($action.toLowerCase()) {
+            case "check":
+                return _Check($element);
+            case "show":
+                return _Show();
+            case "hide":
+                return _Hide();
+        }
+    };
 
-    $loaderDiv.appendTo('body').show();
-}
+    function _Check($element) {
+        if ($($element).hasClass('no-loader')) {
+            return;
+        }
 
-function _Hide() {
-    var $loaders = $('.js-loader');
+        var $checkForm = $($element).data('check-form');
+        if ($checkForm !== undefined) {
+            $form = $('form[name="' + $($element).data('check-form') + '"]');
+            if ($form.length === 0 || $form[0].noValidate || $form[0].checkValidity()) {
+                $.Loader("show");
+                $form[0].submit();
+            }
 
-    if ($loaders.length === 0) {
-        return;
+            return;
+        }
+
+        $.Loader("show");
     }
 
-    $loaders.remove();
-}
+    function _Show() {
+        if ($('.js-loader').length > 0) {
+            return;
+        }
 
+        var $loaderDiv = $("<div>", {
+            "class": "js-loader modal-backdrop",
+            "css": {
+                "display": "flex",
+                "flex-direction": "column",
+                "align-items": "center",
+                "justify-content": "center",
+                "opacity": "0.8"
+            }
+        });
+
+        $('<i>', {
+            'class': 'fa fa-spinner fa-pulse fa-3x fa-fw'
+        }).appendTo($loaderDiv);
+
+        $loaderDiv.appendTo('body').show();
+    }
+
+    function _Hide() {
+        var $loaders = $('.js-loader');
+
+        if ($loaders.length === 0) {
+            return;
+        }
+
+        $loaders.remove();
+    }
+
+}(jQuery));
+
+/**
+ *
+ */
 $(document).ready(function () {
     /** Constant div card */
     const DIV_CARD = 'div.card';
