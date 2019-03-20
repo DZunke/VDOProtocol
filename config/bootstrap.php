da98a045ba7c4c8237e0e8fe7ad57e5498d13012
Denis Zunke <denis.zunke@gmail.com>
2019-03-20 14:51:09 +0100

    Cleanup Yaml Files

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,15 +4,12 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-// Load cached env vars if the .env.local.php file exists
-// Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
     $_SERVER += $env;
     $_ENV += $env;
 } elseif (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 } else {
-    // load all the .env files
     (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
 }
 
