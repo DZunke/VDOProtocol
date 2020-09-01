db27e0f4920c8d205a1b37514c9ad1ef8e8fb381
Denis Zunke <denis.zunke@gmail.com>
2020-09-01 14:03:15 +0200

    Upgrade composer packages

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
 serve-web:
-	symfony serve --no-tls
+	symfony local:server:start --no-tls
 
 check-cs: ## check coding standards
 	vendor/bin/phpcs -n
--- src/Kernel.php
+++ src/Kernel.php
@@ -22,9 +22,11 @@ class Kernel extends BaseKernel
     {
         $contents = require $this->getProjectDir() . '/config/bundles.php';
         foreach ($contents as $class => $envs) {
-            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
-                yield new $class();
+            if (! ($envs[$this->environment] ?? $envs['all'] ?? false)) {
+                continue;
             }
+
+            yield new $class();
         }
     }
 
