daf71bb1446107fe8ce28b98c47fbd7010ed0630
Denis Zunke <denis.zunke@gmail.com>
2021-02-22 18:42:13 +0100

    temporary upgrade to tabler ui 1.0 alpha

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 // any CSS you require will output into a single css file (app.css in this case)
-require('tabler-ui/dist/assets/css/dashboard.css');
+// require('tabler-ui/dist/assets/css/dashboard.css');
+require('@tabler/core/dist/css/tabler.min.css');
 require('./css/app.scss');
 require('./css/fontawesome.min.css');
 require('./css/brands.min.css');
@@ -7,6 +8,7 @@ require('./css/solid.css');
 
 import 'bootstrap';
 import './bootstrap'
+import '@tabler/core';
 
 const $ = require('jquery');
 global.$ = global.jQuery = $;
